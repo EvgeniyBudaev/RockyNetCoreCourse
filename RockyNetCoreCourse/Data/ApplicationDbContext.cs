cc62f8922ae42ee4b6910d7ea7754f7bb8876999
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-05-18 05:50:57 +0300

    Initial commit

---
+++ RockyNetCoreCourse/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RockyNetCoreCourse.Models;
+
+namespace RockyNetCoreCourse.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
